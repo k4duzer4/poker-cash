a5e892a39bf14316a5c3174f11fe5d2e20758c05
Girlando M <patosnetinho@gmail.com>
2026-03-01 12:48:05 -0300

    landing page + melhorias visuais

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { LandingPage } from './pages/LandingPage'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
 
-import { LoginPage } from './pages/LoginPage.tsx'
-import { RegisterPage } from './pages/RegisterPage.tsx'
-
-function App() {
+export const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
 }
